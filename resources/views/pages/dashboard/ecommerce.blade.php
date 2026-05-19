bdef021de0908fea97bcfa0c33d4bc3cae852d95
ollhamm <ilhamkusmayadi65@gmail.com>
2026-05-19 14:24:50 +0700

    ui: remove eccomerce component

--- resources/views/pages/dashboard/ecommerce.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-  <div class="grid grid-cols-12 gap-4 md:gap-6">
-    <div class="col-span-12 space-y-6 xl:col-span-7">
-      <x-ecommerce.ecommerce-metrics />
-    </div>
-  </div>
-@endsection
